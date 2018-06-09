225d2914f8ee172e52061994e8257e2f9eca9c7b
Mark Meyer <mark@ofosos.org>
2018-06-09 15:50:01 +0200

    Overhaul setup.py and include requests dependency

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
-import sys, os
-
 from setuptools import setup, find_packages
 
 long_desc = '''
-This package contains Cloudformation support for Sphinx
+This package contains Cloudformation support for Sphinx.
 
+This package enables you to use the 'cfn' domain to create
+cloudformation resources. It also contains a parser for CloudFormation
+resources to output RST.
+
+See https://github.com/ofosos/cfn-doctemplate for example usage.
 '''
 
-requires = ['Sphinx>=0.6', 'sphinxcontrib-domaintools>=0.1', 'pyyaml']
+requires = ['Sphinx>=0.6',
+            'sphinxcontrib-domaintools>=0.1',
+            'pyyaml',
+            'requests']
 
 setup(
     name='sphinxcontrib-cloudformation',
     version='0.1',
     url='http://github.com/ofosos/sphinx-cfn',
-    download_url='http://pypi.python.org/pypi/sphinxcontrib-cloudformation',
-    license='BSD',
+    download_url='http://pypi.python.org/pypi/cfnsphinx',
+    license='GPLv3',
     author='Mark Meyer',
     author_email='mark@ofosos.org',
-    description='Cloudformation extension for Sphinx',
+    description='Cloudformation domain for Sphinx',
     long_description=long_desc,
     zip_safe=False,
     classifiers=[
@@ -25,7 +31,6 @@ setup(
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Documentation',
@@ -36,7 +41,7 @@ setup(
     include_package_data=True,
     install_requires=requires,
     namespace_packages=['cfnsphinx'],
-    entry_points = {
+    entry_points={
         'console_scripts': ['cfnsphinx-build=cfnsphinx.cfn_build:main'],
     },
 
